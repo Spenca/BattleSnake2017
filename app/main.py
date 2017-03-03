061de08bc3496ce51c6fb838234e59ffa3ba9522
Spencer Vatrt-Watts <asvw@uvic.ca>
2017-03-02 20:11:17 -0800

    Add old repo

---
+++ app/main.py
@@ -0,0 +1,67 @@
+import bottle
+import os
+import random
+
+snakePos = 1
+foodPos = 2
+safePos = 3
+
+# Initialize our personal grid
+def init(data):
+    grid = [ [0 for col in xrange(data['width'])] for row in xrange(data['height']) ]
+
+    for snake in data['snakes']: # Loop through all snakes on the board
+        if snake['id'] == ID: # See if it's us
+            ourSnake = snake
+        for coord in snake['coords']: # Get the coordinates of the snake
+            grid[coord[0]][coord[1]] = snakePos # Set grid val to snake
+
+    for food in data['food']: # Loop through all food on the board
+        grid[food[0]][food[1]] = foodPos
+
+    return ourSnake, grid
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='static/')
+
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+    game_id = data['game_id']
+    board_width = data['width']
+    board_height = data['height']
+
+    head_url = '%s://%s/static/head.png' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+    
+    # TODO: Do things with data
+
+    return {
+        'color': '#00FF00',
+        'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
+        'head_url': head_url,
+        'name': 'battlesnake-python'
+    }
+
+
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+    directions = ['up', 'down', 'left', 'right']
+
+    return {
+        'move': random.choice(directions),
+        'taunt': 'battlesnake-python!'
+    }
+
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+if __name__ == '__main__':
+    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
---
+++ app/utils.py
@@ -0,0 +1,59 @@
+# Get the length of a snake
+def getSnakeLen(coords):
+	return len(coords)
+
+
+# Get the distance between two points
+def distance(p, q):
+    dx = abs(p[0] - q[0])
+    dy = abs(p[1] - q[1])
+    return dx + dy
+
+# Find the closest piece of food in relation to position
+def closestFood(foodList, position):
+	closestFood = None
+	closestDist = 9999
+
+	for food in foodList:
+		dist = distance(food, position)
+		if dist < closestDist:
+			closestFood = food
+			closestDist = dist
+
+	return closestFood
+
+# Get the direction a snake is facing
+def getDirection(snake):
+	fst = snake['coords'][0]
+	snd = snake['coords'][1]
+	dx = fst[0] - snd[0]
+	dy = fst[1] - snd[1]
+
+	if dx == 1:
+		return 'right'
+	elif dx == -1:
+		return 'left'
+	elif dy == 1:
+		return 'down'
+	elif dy == -1:
+		return 'up'
+
+# Get the next cell coordinates based on a snake's direction
+def getNextCell(snake):
+	x1 = snake['coords'][0][0]
+	y1 = snake['coords'][0][1]
+	x2 = snake['coords'][1][0]
+	y2 = snake['coords'][1][1]
+
+	direction = getDirection(snake)
+
+	if direction == 'right':
+		return [x1 + 1, y1]
+	elif direction == 'left':
+		return [x1 - 1, y1]
+	elif direction == 'down':
+		return [x1, y1 + 1]
+	elif direction == 'up':
+		return [x1, y1 - 1]
+
+

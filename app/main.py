8e8a5e6e709cac63b640dfcaa36e2bb6c76caed3
Spencer Vatrt-Watts <asvw@uvic.ca>
2017-03-02 20:36:40 -0800

    Change snake colour

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': '#000000',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'Vulture Snake'
